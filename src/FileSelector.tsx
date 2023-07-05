7dc8ba52702290125dbb33fdcc916aed1a1a1c95
wtlin1228 <wtlin1228@gmail.com>
2023-07-06 00:34:02 +0800

    add select another file section

--- src/FileSelector.tsx
+++ src/FileSelector.tsx
@@ -6,6 +6,7 @@ const getFileNameAndExtension = (
 ): {
   name: string;
   extension: string;
+  errorMessage?: string;
 } => {
   if (file === null) {
     return {
@@ -14,18 +15,12 @@ const getFileNameAndExtension = (
     };
   }
 
-  let lastDotIdx = 0;
-
-  for (let i = file.name.length - 1; i >= 0; i--) {
-    if (file.name[i] === ".") {
-      lastDotIdx = i;
-      break;
-    }
-  }
+  const lastDotIdx = file.name.lastIndexOf(".");
 
   return {
     name: file.name.slice(0, lastDotIdx),
     extension: file.name.slice(lastDotIdx),
+    errorMessage: "error message",
   };
 };
 
@@ -38,63 +33,78 @@ interface IFileSelectorProps {
 export const FileSelector: React.FC<IFileSelectorProps> = (props) => {
   const [isDraggingOver, setIsDraggingOver] = useState(false);
 
-  const { name, extension } = getFileNameAndExtension(props.file);
+  const { name, extension, errorMessage } = getFileNameAndExtension(props.file);
 
   return (
     <div className="file-selector">
-      {props.file !== null ? (
-        <div>
-          <div>{name}</div>
-          <div>{extension}</div>
-        </div>
-      ) : (
-        <div
-          className={`file-selector__dropzone ${
-            isDraggingOver && "file-selector__dropzone--highlight"
-          }`}
-          onDragOver={() => {
-            if (!isDraggingOver) {
-              setIsDraggingOver(true);
-              console.log("drag over");
-            }
-          }}
-          onDragLeave={() => {
-            if (isDraggingOver) {
-              setIsDraggingOver(false);
-              console.log("drag leave");
-            }
-          }}
-          onDrop={() => {
-            setIsDraggingOver(false);
-            console.log("drop");
-          }}
-        >
-          <img className="file-selector__icon" src={CloudUploadSVG} />
-          <label htmlFor="file-selector">
-            drag and drop your file here
-            <br />
-            or, <span className="file-selector__button">select one</span>
-          </label>
-          <input
-            className="file-selector__input"
-            id="file-selector"
-            name="file-selector"
-            type="file"
-            onChange={(e) => {
-              const file = e.target.files?.[0];
-
-              if (!file) {
-                props.onFileClear();
-                return;
-              }
-
-              props.onFileChange(file);
-
-              console.log(getFileNameAndExtension(file));
-            }}
-          />
+      {props.file !== null && (
+        <div className="file-selector__display-wrapper">
+          <div className="file-selector__file-description">
+            <div>{name}</div>
+            <div>{extension}</div>
+            <button
+              className="file-selector__clear-file"
+              onClick={props.onFileClear}
+            >
+              x
+            </button>
+            <label
+              className="file-selector__select-button button-like"
+              htmlFor="file-selector"
+            >
+              choose another file
+            </label>
+          </div>
+          {errorMessage && (
+            <div className="file-selector__file-error">{errorMessage}</div>
+          )}
         </div>
       )}
+
+      <div
+        className={`file-selector__dropzone ${
+          isDraggingOver && "file-selector__dropzone--highlight"
+        } ${props.file !== null && "file-selector__dropzone--hidden"}`}
+        onDragOver={() => {
+          if (!isDraggingOver) {
+            setIsDraggingOver(true);
+            console.log("drag over");
+          }
+        }}
+        onDragLeave={() => {
+          if (isDraggingOver) {
+            setIsDraggingOver(false);
+            console.log("drag leave");
+          }
+        }}
+        onDrop={() => {
+          setIsDraggingOver(false);
+          console.log("drop");
+        }}
+      >
+        <img className="file-selector__icon" src={CloudUploadSVG} />
+        <label htmlFor="file-selector">
+          drag and drop your file here
+          <br />
+          or, <span className="file-selector__select-button">select one</span>
+        </label>
+        <input
+          className="file-selector__input"
+          id="file-selector"
+          name="file-selector"
+          type="file"
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+
+            if (!file) {
+              props.onFileClear();
+              return;
+            }
+
+            props.onFileChange(file);
+          }}
+        />
+      </div>
     </div>
   );
 };
